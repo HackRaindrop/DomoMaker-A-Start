cb7416d56533a24e2108172708d8433194ff3466
HackRaindrop <jwkirk15@gmail.com>
2025-11-05 14:50:56 -0500

    Done up to step 14 of part b

--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -1,7 +1,35 @@
-const makerPage = (req, res) => {
-    res.render('app');
-};
+const mongoose = require('mongoose');
+const _ = require('underscore');
 
-module.exports = {
-    makerPage,
-};
+const setName = (name) => _.escape(name).trim();
+
+const DomoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  age: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account',
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+DomoSchema.statics.toAPI = (doc) => ({
+  name: doc.name,
+  age: doc.age,
+});
+
+const DomoModel = mongoose.model('Domo', DomoSchema);
+module.exports = DomoModel;
